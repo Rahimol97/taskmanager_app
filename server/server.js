c02aae0b2b771c2b27904fb28c891d35f1f55fb4
rahi <rahimolrg@gmail.com>
2025-12-28 18:05:28 +0530

    error

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ connectDB();
 const app = express();
 app.use(cookieParser());
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://taskmanager-app-smoky.vercel.app",
   credentials: true
 })); 
 
